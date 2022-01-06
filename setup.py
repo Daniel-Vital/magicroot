c1b123ed6819152f11c8a4ca59e95e4d552661a6
ATREMA\daalcantara <daalcantara@deloitte.pt>
2022-01-06 12:10:18 +0000

    Updated setupfile

--- setup.py
+++ setup.py
@@ -4,18 +4,23 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='magicroot',
-    version='0.0.1',
-    author='Daniel Vital',
-    author_email='daniel.alcantara96@gmail.com',
-    description='Testing installation of Package',
+    name="Daniel-Vital",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/mike-huls/toolbox',
-    project_urls = {
-        "Bug Tracker": "https://github.com/mike-huls/toolbox/issues"
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
     },
-    license='MIT',
-    packages=['toolbox'],
-    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
