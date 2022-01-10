809c980cd9d87e3b174ae0ae545e65fea2e1b8ea
ATREMA\daalcantara <daalcantara@deloitte.pt>
2022-01-10 16:51:21 +0000

    implemented way of printing first lines of tables and files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="magicroot",
-    version="1.2.3",
+    version="1.3.3",
     author="Daniel-Vital",
     author_email="author@example.com",
     description="A small example package",
