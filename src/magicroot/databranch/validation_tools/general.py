53be0f958f48679d147ff26037ebfd08ead24b80
Daniel Vital de Alcântara <daalcantara@deloitte.pt>
2022-04-28 08:33:36 +0100

    Added validation tools package

---
+++ src/magicroot/databranch/validation_tools/general.py
@@ -0,0 +1,36 @@
+
+
+def check_indexes(df, index):
+    """
+    Returns all duplicated lines of a dataframe for a given index
+    :param df:
+    :param index:
+    :return:
+    """
+    return df[df[index].duplicated(keep=False)].sort_values(index)
+
+
+def compared_grouped(df_base, df_new, by, target_columns, diff_columns_suffixe='DIFF'):
+    """
+    Compares two grouped versions of a pair of dataframes
+    :param df_base:
+    :param df_new:
+    :return:
+    """
+    def group_func(df):
+        return df.groupby(by).sum().reset_index()
+
+    df_base = group_func(df_base)
+    df_new = group_func(df_new)
+
+    def diff(column):
+        return lambda df: (df[column] - df[column + '_new']).abs()
+
+    return df_base.merge(df_new, how='outer', validate='one_to_one', suffixes=('', '_new')).assign(
+        **{
+            diff_columns_suffixe + target_column: diff(target_column)
+            for target_column in target_columns
+        }
+    ).sort_values(by=[diff_columns_suffixe + target_column for target_column in target_columns], ascending=False)
+
+
