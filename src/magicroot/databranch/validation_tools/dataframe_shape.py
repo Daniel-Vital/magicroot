d97004acac19c6928831b7630f9778a128e770ce
Daniel Vital de Alcântara <daalcantara@deloitte.pt>
2022-05-04 15:11:15 +0100

    Added get_numeric_columns to dataframe_shape.py

--- src/magicroot/databranch/validation_tools/dataframe_shape.py
+++ src/magicroot/databranch/validation_tools/dataframe_shape.py
@@ -14,3 +14,18 @@ def uniformize_columns(*args):
                 df[column] = np.nan
     return args
 
+
+def get_numeric_columns(df, invert=False, remove=None):
+    """
+    Return a list of the numeric columns of a Dataframe
+    :param df:
+    :param invert: Modifier to return a list of the non-numeric columns of a Dataframe
+    :param remove: Columns to remove from output
+    :return:
+    """
+    if invert:
+        columns = df.select_dtypes(exclude=np.number).columns.to_list()
+    else:
+        columns = df.select_dtypes(include=np.number).columns.to_list()
+    columns.remove(remove)
+    return columns
