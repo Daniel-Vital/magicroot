17b54607dbec9f96a4a3ea781461e98949015238
Daniel Vital de Alcântara <daalcantara@deloitte.pt>
2022-05-09 19:00:20 +0100

    Simplified creation of new parcers.py

--- src/magicroot/databranch/directorymanager/parcers.py
+++ src/magicroot/databranch/directorymanager/parcers.py
@@ -1,33 +1,15 @@
-import json
+
 import os.path
 
 import pandas as pd
-from .parcer import Parser
+from .parcer_base import Parser, JSON
 import logging
 
 log = logging.getLogger('MagicRoot.databranch.directorymanager.parcers')
 
 
-class JSON(Parser):
-    extension = '.json'
-
-    def read(self, *args, **kwargs):
-        log.debug(f'Reading .json \'{self.path}\'')
-        with open(self.path, 'r') as f:
-            return json.load(f)
-
-    def save(self, obj, *args, **kwargs):
-        log.debug(f'Saving .json \'{self.path}\'')
-        with open(self.path, 'w+') as outfile:
-            json.dump(obj, outfile)
-
-    def peak(self, *args, **kwargs):
-        log.debug(f'Peaking .json \'{self.path}\'')
-        return self.read(*args, **kwargs).__repr__()
-
-
 class CSV(Parser):
-    extension = '.csv'
+    extension = 'csv'
 
     def read(self, *args, **kwargs):
         return pd.read_csv(filepath_or_buffer=self.path, *args, **self.read_settings(**kwargs))
@@ -38,19 +20,9 @@ class CSV(Parser):
     def peak(self, *args, **kwargs):
         return self.read(nrows=5, *args, **kwargs).__repr__()
 
-    @classmethod
-    def set_default_settings(cls, settings):
-        path = os.path.join(cls.settings_path, 'csv.json')
-        JSON(path).save(settings)
-
-    @classmethod
-    def load_default_settings(cls):
-        path = os.path.join(cls.settings_path, 'csv.json')
-        return JSON(path).read()
-
 
 class SAS(Parser):
-    extension = '.sas7bdat'
+    extension = 'sas7bdat'
 
     def read(self, *args, **kwargs):
         return pd.read_sas(filepath_or_buffer=self.path, *args, **self.read_settings(**kwargs))
@@ -60,14 +32,4 @@ class SAS(Parser):
 
     def peak(self, *args, **kwargs):
         return self.read(encoding='latin-1', *args, **kwargs).__repr__()
-
-    @classmethod
-    def set_default_settings(cls, settings):
-        path = os.path.join(cls.settings_path, 'sas.json')
-        JSON(path).save(settings)
-
-    @classmethod
-    def load_default_settings(cls):
-        path = os.path.join(cls.settings_path, 'sas.json')
-        return JSON(path).read()
 
