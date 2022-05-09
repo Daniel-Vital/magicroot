a373e68ee2c380e9bd278bc56e52905297e9211a
Daniel Vital de Alcântara <daalcantara@deloitte.pt>
2022-05-09 13:43:46 +0100

    Fixed home variable

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="magicroot",
-    version="0.1.37",
+    version="0.1.38",
     author="Daniel-Vital",
     author_email="author@example.com",
     description="Python like magic",
--- src/magicroot/__init__.py
+++ src/magicroot/__init__.py
@@ -1,10 +1,14 @@
 
+# modules
 from . import databranch
 from . import diamondpinky
 from . import fileleaf
 from . import pysas
 from . import gardeningtools
 from . import smartowl
+
+# global variables
+from .databranch import home
 
 import logging
 logging.getLogger('magicroot').addHandler(logging.NullHandler())
