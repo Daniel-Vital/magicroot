e49a38aa5415179d4c70f6ad72714b407702c85d
Daniel Vital de Alcântara <daalcantara@deloitte.pt>
2022-05-09 09:43:32 +0100

    Implemented function to get extension on extensions.py

--- src/magicroot/fileleaf/extensions.py
+++ src/magicroot/fileleaf/extensions.py
@@ -14,3 +14,15 @@ def change_file_extension(path, new_extension):
     head, file_name, extension = split_path(path)
     shutil.copy2(path, os.path.join(head, file_name + new_extension))
 
+
+def get(path):
+    """
+    Gets the extension of a file
+    :param path: complete path to the file (including the file and extension)
+    Example:
+    >>> r"C:/Users/some_user/Documents/some_file.csv"
+    :return: Extension
+    """
+    _, _, extension = split_path(path)
+    return extension
+
