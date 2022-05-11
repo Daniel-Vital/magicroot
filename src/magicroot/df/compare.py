693bc9891c7e0787e0b82eaded7dffcd4546b427
Daniel Vital de Alcântara <daalcantara@deloitte.pt>
2022-05-11 20:43:24 +0100

    Updated compare.py grouped to use dataframe

--- src/magicroot/df/compare.py
+++ src/magicroot/df/compare.py
@@ -3,34 +3,22 @@ from .reshape import uniformize_columns
 from .select import numeric_columns
 
 
-def grouped(df_base, df_new, by, target_columns, diff_columns_suffixe='DIFF_'):
+def grouped(df_tested, df_benchmark, by, *args, **kwargs):
     """
     Compares two grouped versions of a pair of dataframes
-    :param df_base:
-    :param df_new:
+    :param df_tested:
+    :param df_benchmark:
     :param by:
-    :param target_columns:
-    :param diff_columns_suffixe:
     :return:
     """
 
     def group_func(df):
         return df.groupby(by).sum().reset_index()
 
-    df_base = group_func(df_base)
-    df_new = group_func(df_new)
+    df_tested = group_func(df_tested)
+    df_benchmark = group_func(df_benchmark)
 
-    def diff(column):
-        return lambda df: df[column + '_new'] - df[column]
-
-    return df_base.merge(df_new, how='outer', validate='one_to_one', suffixes=('', '_new'), on=by).fillna(0).assign(
-        **{
-            diff_columns_suffixe + target_column: diff(target_column)
-            for target_column in target_columns
-        }
-    ).sort_values(
-        by=[diff_columns_suffixe + target_column for target_column in target_columns], ascending=False
-    )
+    return dataframes(df_tested, df_benchmark, by, *args, **kwargs)
 
 
 def types(*args):
