04fd18d796daf25e528ae23cd5cca6acaf373d67
Daniel Vital de Alcântara <daalcantara@deloitte.pt>
2022-05-27 15:29:34 +0100

    Added funcitionality for coping files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="magicroot",
-    version="0.1.61",
+    version="0.1.62",
     author="Daniel-Vital",
     author_email="author@example.com",
     description="Python like magic",
