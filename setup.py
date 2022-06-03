cdde81fc7ca08a33b9ebf7b5e2b155e688e1f0d4
Daniel Vital de Alcântara <daalcantara@deloitte.pt>
2022-06-03 21:11:10 +0100

    Implemented index.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="magicroot",
-    version="0.1.66",
+    version="0.1.67",
     author="Daniel-Vital",
     author_email="author@example.com",
     description="Python like magic",
