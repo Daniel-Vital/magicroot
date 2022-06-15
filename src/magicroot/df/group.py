2fa4191af22becf8b24f73e850b1db622b5ec855
Daniel Vital de Alcântara <daalcantara@deloitte.pt>
2022-06-15 10:37:26 +0100

    implemented group.py

--- src/magicroot/df/group.py
+++ src/magicroot/df/group.py
@@ -1,7 +1,7 @@
 
 
 def cumsum(df, columns, by, order):
-    return df.sort_values(order)[by + columns].groupby(by).cumsum()
+    return df.sort_values(by + order)[by + columns].groupby(by).cumsum()
 
 
 def sum(df, columns, by):
